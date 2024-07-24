5dbd8cfd19cdcab016e3eb2f21f8930256643aa5
Volodymyr Demchuk <demchukv@gmail.com>
2024-07-24 20:24:26 +0300

    add pages: login, register, about

---
+++ src/app/about/page.js
@@ -0,0 +1,7 @@
+export default function Register() {
+  return (
+    <main>
+      <h1>About page</h1>
+    </main>
+  );
+}
---
+++ src/app/register/page.js
@@ -0,0 +1,10 @@
+import RegisterForm from '../components/RegisterForm/RegisterForm';
+
+export default function Register() {
+  return (
+    <main>
+      <h1>Register page</h1>
+      <RegisterForm />
+    </main>
+  );
+}
