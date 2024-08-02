2c1711286af7a68165e3434b0b2b1efe61220b02
Volodymyr Demchuk <demchukv@gmail.com>
2024-08-02 21:43:28 +0300

    add /data/user-invitations.ts

---
+++ data/user-invitations.ts
@@ -0,0 +1,27 @@
+import { db } from "@/lib/db";
+
+export const getUserInvitationsList = async (userId: string) => {
+  try {
+    const userInvitations = await db.invite.findMany({
+      where: {
+        userId,
+      },
+    });
+    return userInvitations;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getUserInvitationById = async (id: string) => {
+  try {
+    const userInvitation = await db.invite.findUnique({
+      where: {
+        id,
+      },
+    });
+    return userInvitation;
+  } catch (error) {
+    return null;
+  }
+};
