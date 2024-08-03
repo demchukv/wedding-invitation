3e13e34a9f73f692b0ca1750ba2053744c4f305d
Volodymyr Demchuk <demchukv@gmail.com>
2024-08-03 10:29:22 +0300

    add invitations list

--- components/invitations/invitations-list-item.tsx
+++ components/invitations/invitations-list-item.tsx
@@ -1,9 +1,29 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 const InvitationsListItem = ({ invitation }: { invitation: any }) => {
   return (
-    <div>
-      <p>
+    <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+      <Link href={`/invitations/${invitation.id}`}>
         {invitation.nameOne} + {invitation.nameTwo}
-      </p>
+      </Link>
+      <div className="flex gap-2">
+        <Button variant="default" size="sm">
+          <Link href={`/invitations/${invitation.id}`}>
+            <CiEdit />
+          </Link>
+        </Button>
+        <Button
+          variant="default"
+          size="sm"
+          onClick={() => {
+            alert("make delete");
+          }}
+        >
+          <MdDeleteOutline />
+        </Button>
+      </div>
     </div>
   );
 };
