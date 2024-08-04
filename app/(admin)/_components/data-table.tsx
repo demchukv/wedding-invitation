92687b4a1c3a2226fa6cad596151d0a96ed40924
Volodymyr Demchuk <demchukv@gmail.com>
2024-08-04 20:42:31 +0300

    add server-side sorting for data table

--- app/(admin)/_components/data-table.tsx
+++ app/(admin)/_components/data-table.tsx
@@ -86,6 +86,7 @@ export function DataTable<TData, TValue>({
     },
     pageCount: rowCount === 0 ? 0 : Math.ceil(rowCount / pagination.pageSize),
     rowCount: rowCount,
+    manualSorting: true,
     manualPagination: true,
     autoResetPageIndex: false,
   });
