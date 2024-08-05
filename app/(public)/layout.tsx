95f8b5b1b5f65c45f352e501313c5d23f8e22661
Volodymyr Demchuk <demchukv@gmail.com>
2024-08-05 13:31:41 +0300

    add AuthPrivider to layouts

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,16 +1,17 @@
 import { TopNavigation } from "@/components/top-navigation";
+import { AuthProvider } from "@/components/auth-provider";
 
 interface ProtectedLayoutProps {
   children: React.ReactNode;
 }
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
   return (
-    <>
+    <AuthProvider>
       <TopNavigation />
       <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
         {children}
       </div>
-    </>
+    </AuthProvider>
   );
 };
 
