30d8787cabdc361ce36620edad1b3566525d543e
Svitlana Kostohryz <142225792+SvKost@users.noreply.github.com>
2024-08-14 15:25:39 +0300

    ReviewsList created

--- data/review.ts
+++ data/review.ts
@@ -1,6 +1,22 @@
 'use server'
 
 import { db } from "@/lib/db"
+import { ReviewType } from "@/types/review";
+
+export const getAllReviews = async (): Promise<ReviewType[] | {error:string}> => {
+  try {
+    const reviews = await db.review.findMany({
+      take: 3,
+      orderBy: {
+        createdAt: "desc",
+      }
+    });
+    return reviews;
+  } catch (error) {
+    console.log(error);
+    return { error: "Error fetching reviews. " + error };
+  }
+};
 
 export const getReviewById = async (id: string) => {
     try {
