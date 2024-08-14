5771fdd56ba7e658fabbd457bc42060e820d8114
Volodymyr Demchuk <demchukv@gmail.com>
2024-08-14 12:03:45 +0300

    save data on page leave or change tab

--- app/(protected)/invitations/[id]/page.tsx
+++ app/(protected)/invitations/[id]/page.tsx
@@ -11,30 +11,36 @@ import {
 } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import { useEffect, useState, useTransition } from "react";
+import { useEffect, useState, useTransition, useRef } from "react";
 import { getUserInvitationById } from "@/data/user-invitations";
 import { InvitationType } from "@/types/invitation";
 import { BeatLoader } from "react-spinners";
 import dynamic from "next/dynamic";
+import {
+  EditInvitation,
+  EditRef,
+} from "@/app/(protected)/_components/invitation/edit-invitation";
 
-const EditInvitation = dynamic(() =>
-  import("@/app/(protected)/_components/invitation/edit-invitation").then(
-    mod => mod.EditInvitation
-  )
-);
+// const EditInvitation = dynamic(() =>
+//   import("@/app/(protected)/_components/invitation/edit-invitation").then(
+//     mod => mod.EditInvitation
+//   )
+// );
 
 const InvitePage = ({ params }: { params: { id: string } }) => {
   const [isPending, startTransition] = useTransition();
   const [data, setData] = useState<InvitationType | null>();
   const [tab, setTab] = useState("account");
-  const [save, setSave] = useState(false);
+  const [reloadData, setReloadData] = useState(false);
   const id = params.id;
+  const editRef = useRef<EditRef>(null);
 
   const onTabSwitch = (value: string) => {
     if (tab === "account" && value !== "account") {
-      setSave(true);
-    } else {
-      setSave(false);
+      editRef?.current?.onTabChangeSaveData();
+    }
+    if (tab !== "account" && value === "account") {
+      setReloadData(true);
     }
     setTab(value);
   };
@@ -43,12 +49,14 @@ const InvitePage = ({ params }: { params: { id: string } }) => {
     const getInvitation = async (id: string) => {
       startTransition(() => {
         getUserInvitationById(id).then(res => {
+          console.log("Load data from DB");
           setData(res);
+          setReloadData(false);
         });
       });
     };
     getInvitation(id);
-  }, [id]);
+  }, [id, reloadData]);
 
   return (
     <>
@@ -76,7 +84,7 @@ const InvitePage = ({ params }: { params: { id: string } }) => {
                 <CardDescription>Edit your invitation</CardDescription>
               </CardHeader>
               <CardContent className="space-y-2">
-                <EditInvitation data={data} save={save} />
+                <EditInvitation data={data} ref={editRef} />
               </CardContent>
             </Card>
           </TabsContent>
