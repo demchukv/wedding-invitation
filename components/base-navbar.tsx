3e6946a472af92bb97f133a0435c91b410fc2cac
Volodymyr Demchuk <demchukv@gmail.com>
2024-08-18 20:27:19 +0300

    add logo

--- components/base-navbar.tsx
+++ components/base-navbar.tsx
@@ -7,6 +7,7 @@ import {
   NavigationMenuItem,
 } from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
+import { cormorant } from "@/styles/fonts";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -14,49 +15,65 @@ export const BaseNavbar = () => {
   const pathname = usePathname();
 
   return (
-    <NavigationMenu>
-      <NavigationMenuList className="flex-col sm:flex-row sm:gap-3">
-        <NavigationMenuItem
-          className={cn(
-            "text-mblack font-normal text-xl border-transparent border-[2px] hover:border-mpink active:bg-mpink focus:bg-mpink transition-all px-3 py-2 rounded-[2px]",
-            pathname === "/" && "border-mlightbrown bg-mlightbrown"
-          )}
-        >
-          <Link href="/" legacyBehavior passHref>
-            <NavigationMenuLink>Home</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem
-          className={cn(
-            "text-mblack font-normal text-xl border-transparent border-[2px] hover:border-mpink active:bg-mpink focus:bg-mpink transition-all px-3 py-2 rounded-[2px]",
-            pathname === "/about" && "border-mlightbrown bg-mlightbrown"
-          )}
-        >
-          <Link href="/about" legacyBehavior passHref>
-            <NavigationMenuLink>About</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem
-          className={cn(
-            "text-mblack font-normal text-xl border-transparent border-[2px] hover:border-mpink active:bg-mpink focus:bg-mpink transition-all px-3 py-2 rounded-[2px]",
-            pathname === "/templates" && "border-mlightbrown bg-mlightbrown"
-          )}
-        >
-          <Link href="/templates" legacyBehavior passHref>
-            <NavigationMenuLink>Templates</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem
-          className={cn(
-            "text-mblack font-normal text-xl border-transparent border-[2px] hover:border-mpink active:bg-mpink focus:bg-mpink transition-all px-3 py-2 rounded-[2px]",
-            pathname === "/feedback" && "border-mlightbrown bg-mlightbrown"
-          )}
-        >
-          <Link href="/feedback" legacyBehavior passHref>
-            <NavigationMenuLink>Contact Us</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <>
+      <NavigationMenu>
+        <NavigationMenuList className="flex-col items-center sm:flex-row sm:gap-3">
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link
+                className={cn(
+                  cormorant.className,
+                  "no-underline outline-none text-mblack pr-16 text-[24px] leading-8 font-normal"
+                )}
+                href="/"
+              >
+                InviTEAM
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem
+            className={cn(
+              "text-mblack font-normal text-xl border-transparent border-[2px] hover:border-mpink active:bg-mpink focus:bg-mpink transition-all px-3 py-2 rounded-[2px]",
+              pathname === "/" && "border-mlightbrown bg-mlightbrown"
+            )}
+          >
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink>Home</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem
+            className={cn(
+              "text-mblack font-normal text-xl border-transparent border-[2px] hover:border-mpink active:bg-mpink focus:bg-mpink transition-all px-3 py-2 rounded-[2px]",
+              pathname === "/about" && "border-mlightbrown bg-mlightbrown"
+            )}
+          >
+            <Link href="/about" legacyBehavior passHref>
+              <NavigationMenuLink>About</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem
+            className={cn(
+              "text-mblack font-normal text-xl border-transparent border-[2px] hover:border-mpink active:bg-mpink focus:bg-mpink transition-all px-3 py-2 rounded-[2px]",
+              pathname === "/templates" && "border-mlightbrown bg-mlightbrown"
+            )}
+          >
+            <Link href="/templates" legacyBehavior passHref>
+              <NavigationMenuLink>Templates</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem
+            className={cn(
+              "text-mblack font-normal text-xl border-transparent border-[2px] hover:border-mpink active:bg-mpink focus:bg-mpink transition-all px-3 py-2 rounded-[2px]",
+              pathname === "/feedback" && "border-mlightbrown bg-mlightbrown"
+            )}
+          >
+            <Link href="/feedback" legacyBehavior passHref>
+              <NavigationMenuLink>Contact Us</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </>
   );
 };
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -9,7 +9,7 @@ export const Header = async () => {
   const session = await auth();
   return (
     <header>
-      <div className="container mx-auto flex flex-row items-center justify-between w-full py-4 lg:[background:url(/icons/bg/head-lg-bg.svg)_132px_top_no-repeat]">
+      <div className="container mx-auto flex flex-row items-center justify-between w-full py-4 sm:[background:url(/icons/bg/head-md-bg.svg)_115px_top_no-repeat] lg:[background:url(/icons/bg/head-lg-bg.svg)_132px_top_no-repeat]">
         <BaseNavbar />
         {!session ? <NewUserMenu /> : <Navbar />}
       </div>
